3d6ef77f20fe547654cf02b1fb7b97b50f4154d5
ayushr2 <ayushr2@illinois.edu>
2017-02-18 14:11:10 -0600

    update

---
+++ mainsite/static/js/functions.js
@@ -0,0 +1,19 @@
+openOfficialSite = function()
+{
+ window.open("https://goodcitychicago.org/investchicago/");
+};
+
+openLearnMore =  function() {
+window.open("https://goodcitychicago.org/donations/invest/");
+}
+
+openDonate =  function() {
+window.open("https://goodcitychicago.org/donate/");
+}
+
+openTimeTalents = function() {  window.open("https://goodcitychicago.org/volunteer/");
+}
+
+openPortfolioSite = function() {
+    window.open("http://investchicago.org/");
+}
