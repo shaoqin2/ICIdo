40f9fd095eab86ec9597e0f6b470d4f2d56bbbd1
begovic2 <begovic2@illinois.edu>
2017-02-18 12:35:51 -0600

    adding forum models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+#from mainsite.models import Category
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    brief = models.CharField(max_length=500)
+    link = models.URLField(blank=True)
+    # not to display
+    categories = models.ManyToManyFields(Category)
+
+    # internal use only
+    haveToHost = models.BooleanField()
+
+    # if haveToHost, need these fields
+    affiliation = models.CharField(max_length=100, blank=True)
+    description = models.CharField(max_length=5000, blank=True)
+
+
+#remove once done referencing:
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return self.name
