36e3b7f50b2357e3d77b3a296e9a4f67432e1f0b
shaoqin2 <shaoqin2@illinois.edu>
2017-02-19 16:46:35 +0000

    forum done

--- forum/views.py
+++ forum/views.py
@@ -13,4 +13,8 @@ def display_category_events(request, param):
 
 
 def forum_home(request):
-    return render(request, 'Forum.html')
+    d = {}
+    results = Event.objects.all()
+    d['events'] = results
+    print(results)
+    return render(request, 'Forum.html', d)
