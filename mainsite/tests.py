01cb2288e82ae1f5dfa9c18ae7a237b9d3645e2c
begovic2 <begovic2@illinois.edu>
2017-02-18 20:42:22 -0600

    Beginning work on test cases

--- mainsite/tests.py
+++ mainsite/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from mainsite.models import Portfolio
+from mainsite.models import Category
+from mainsite.models import User
+from mainsite.models import Donor
+from mainsite.models import Donation
+
 
 # Create your tests here.
+class PortfolioTest(TestCase):
+    def setUp(self):
+        Portfolio.name.create(name="profolio")
+        Portfolio.name.create(name="Women's Fund")
+
+    def test_portfolio_to_str(self):
+        pro = Portfolio.__getattribute__(self, )
